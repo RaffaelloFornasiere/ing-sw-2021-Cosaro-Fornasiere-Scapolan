30f4e89af3cdfb0be52fbb5c9952cee3e21b014f
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-04-06 22:34:34 +0200

    Added a constructor to LeaderCard

--- src/main/java/it/polimi/ingsw/model/LeaderCards/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/model/LeaderCards/LeaderCard.java
@@ -6,6 +6,12 @@ public class LeaderCard {
     private Requirement activationRequirement;
     private LeaderPower power;
 
+    public LeaderCard(int victoryPoints, Requirement activationRequirement, LeaderPower power){
+        this.victoryPoints = victoryPoints;
+        this.activationRequirement = activationRequirement;
+        this.power = power;
+    }
+
     public int getVictoryPoints() {
         return victoryPoints;
     }
