ce14724b0ada335e87bf70cd70bf146a47ab145c
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-04-08 21:35:31 +0200

    Corrected the documentation for LeaderPower.java

--- src/main/java/it/polimi/ingsw/model/LeaderCards/LeaderPower.java
+++ src/main/java/it/polimi/ingsw/model/LeaderCards/LeaderPower.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.model.LeaderCards;
 
 /**
  * Class containing the data needed to use the ability of a leader card
- * This class can decorate another LeaderPower class to add another power to the same leader card
+ * This class family can decorate another LeaderPower object to add another power to the same leader card
  */
 public abstract class LeaderPower {
 
@@ -17,7 +17,7 @@ public abstract class LeaderPower {
 
     /**
      * Getter for the decorated LeaderPower
-     * @return the LeaderPower that it's being decorated by this class or null if it is not decorating any LeaderPower
+     * @return the LeaderPower that it's being decorated by this class or null if it is not decorating any other LeaderPower
      */
     public LeaderPower getDecoratedLeaderPower() {
         return decoratedLeaderPower;
