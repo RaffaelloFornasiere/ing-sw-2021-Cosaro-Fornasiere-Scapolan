6aed7a9a4cc397df494c61928c07a3fd4097a69b
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-05-06 10:57:07 +0200

    Edited Requirement.java and its subclasses
    Added methods to check if a player meets the requirements described in each class and to see if two requirements are equivalent and to merge two equivalent requirements

--- src/main/java/it/polimi/ingsw/model/LeaderCards/Requirement.java
+++ src/main/java/it/polimi/ingsw/model/LeaderCards/Requirement.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model.LeaderCards;
 
-import com.google.gson.annotations.SerializedName;
+import it.polimi.ingsw.exceptions.IllegalOperation;
+import it.polimi.ingsw.model.Player;
 
 import java.io.Serializable;
 
@@ -8,5 +9,9 @@ import java.io.Serializable;
  * Class containing the requirements for activating a leader card
  */
 public abstract class Requirement implements Serializable {
+
+    public abstract boolean isEquivalent(Requirement other);
+    public abstract Requirement merge(Requirement other) throws IllegalOperation;
+    public abstract boolean checkRequirement(Player p);
 
 }
