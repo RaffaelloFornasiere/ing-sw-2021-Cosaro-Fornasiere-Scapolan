40474dbc93e88f3ecf52efe5dab7cf6b97ec08a2
RaffaelloFornasiere <fornasiere.raffaello@gmail.com>
2021-05-07 18:04:54 +0200

    bug fix

--- src/main/java/it/polimi/ingsw/events/Event.java
+++ src/main/java/it/polimi/ingsw/events/Event.java
@@ -3,14 +3,15 @@ package it.polimi.ingsw.events;
 public abstract class Event {
     int playerId;
 
-    public Event(){}
+    public Event() {
+    }
+
     public int getPlayerId() {
         return playerId;
     }
 
-    public String getEventName()
-    {
-        return this.getClass().getName();
+    public String getEventName() {
+        return this.getClass().getSimpleName();
     }
 
 }
