b180cbbc41114c8967cff43da10393385f292231
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-05-08 21:53:14 +0200

    Created some new exception
    Added the exceptions IncompatiblePowersException, LeaderCardNotActiveException and RequirementsNotMetException

---
+++ src/main/java/it/polimi/ingsw/exceptions/LeaderCardNotActiveException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class LeaderCardNotActiveException extends Throwable {
+    public LeaderCardNotActiveException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/it/polimi/ingsw/exceptions/RequirementsNotMetException.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.exceptions;
+
+public class RequirementsNotMetException extends Throwable {
+    public RequirementsNotMetException(String s) {
+        super(s);
+    }
+}
