57a772609bab9b5449a1dcb106a9513523b93e7d
LisaCosaro <lisa.cosaro@mail.polimi.it>
2021-05-10 11:36:01 +0200

    more tests on FaithTrack()

---
+++ src/main/java/it/polimi/ingsw/events/IncrementedFaithTrackPositionEvent.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.events;
+
+public class IncrementedFaithTrackPositionEvent extends MatchEvent{
+    private int newPosition;
+
+    /**
+     * Constructor for the class
+     * @param playerID the player that generated(directly or indirectly) this event
+     * @param position the updated new position
+     * @throws IllegalArgumentException if the index is negative
+     */
+    public IncrementedFaithTrackPositionEvent (String playerID, int position) throws IllegalArgumentException{
+        super(playerID);
+        if(position<0)
+            throw new IllegalArgumentException("position must be positive");
+        this.newPosition = position;
+    }
+
+    /**
+     * getter for the index of the leader card to activate
+     * @return the index of the leader card to activate
+     */
+    public int getNewPosition() {
+        return newPosition;
+    }
+}
