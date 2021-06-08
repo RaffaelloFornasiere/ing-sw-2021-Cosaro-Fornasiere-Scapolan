4f31b5527ee640f06f4c626288399f55f779ff1a
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-06-08 17:57:08 +0200

    Clarified description of turn states

--- src/main/java/it/polimi/ingsw/model/TurnState.java
+++ src/main/java/it/polimi/ingsw/model/TurnState.java
@@ -3,8 +3,8 @@ package it.polimi.ingsw.model;
 public enum TurnState {
     WAITING_FOR_PLAYER(1, " is waiting for another player"),
     START(2, "is starting "),
-    AFTER_LEADER_CARD_ACTION(3, "has just performed a leader card action"),
-    AFTER_MAIN_ACTION(4, " has just done its action"),
+    AFTER_LEADER_CARD_ACTION(3, "has just performed a leader card action, but can still do a main action"),
+    AFTER_MAIN_ACTION(4, " has just done its action, but can still do the leader action"),
     END_OF_TURN(5, "has ended their turn"),
     WAITING_FOR_SOMETHING(6, "is waiting for something"),
     MATCH_ENDED(7, "has ended the match");
