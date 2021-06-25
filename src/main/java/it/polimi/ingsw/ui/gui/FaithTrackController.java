3960458d2936badc7f8e737a5a0dc01777e6c6e1
RaffaelloFornasiere <fornasiere.raffaello@gmail.com>
2021-06-25 15:13:28 +0200

    FaithTrackController update

--- src/main/java/it/polimi/ingsw/ui/gui/FaithTrackController.java
+++ src/main/java/it/polimi/ingsw/ui/gui/FaithTrackController.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.ui.gui;
 
+import it.polimi.ingsw.model.FaithTrack.PopeFavorCard;
 import javafx.animation.SequentialTransition;
 import javafx.animation.TranslateTransition;
 import javafx.scene.Group;
@@ -14,6 +15,7 @@ import org.javatuples.Triplet;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
@@ -156,6 +158,10 @@ public class FaithTrackController {
         setPosition(position - 1);
     }
 
+    public void setPopeFavorCards(HashMap<Integer, PopeFavorCard> popeFavorCards)
+    {
+
+    }
 
 
 }
