61d0363b867fb2030de1987784ed15e43f57ee84
Leonardo Scapolan <64856855+LeonardoScapolan@users.noreply.github.com>
2021-06-25 12:04:04 +0200

    Implemented the update methods of the UI interface in the GUI
    Changed how leader powers deposits are internally represented in the GUI

--- src/main/java/it/polimi/ingsw/ui/gui/PlayerState.java
+++ src/main/java/it/polimi/ingsw/ui/gui/PlayerState.java
@@ -16,7 +16,9 @@ public class PlayerState {
 
     ArrayList<DepotState> warehouse;
     HashMap<Resource, Integer> strongBox;
-    HashMap<Resource, Integer> leaderDepots;
+
+    private final HashMap<Resource, Integer> leaderDepots;
+    private final HashMap<String, ArrayList<HashMap<Resource, Integer>>> leaderDepotsState;
 
     ArrayList<ArrayList<String>> ownedCards;
 
@@ -25,7 +27,8 @@ public class PlayerState {
     private int faithTrackPosition;
     private HashMap<String, HashMap<Integer, PopeFavorCard>> popeFavorCards;
 
-    ArrayList<String> leaderCards;
+    HashMap<String, Boolean> leaderCards;
+    HashMap<String, ArrayList<Boolean>> leaderPowerStates;
     Integer victoryPoints;
     Integer faithTrackPoints;
     static Pair<Marble[][], Marble> marketStatus;
@@ -38,13 +41,19 @@ public class PlayerState {
     public PlayerState() {
         warehouse = new ArrayList<>();
         strongBox = new HashMap<>();
+        leaderDepots = new HashMap<>();
+        for(Resource r: Resource.values()){
+            leaderDepots.put(r, 0);
+        }
+        leaderDepotsState = new HashMap<>();
         ownedCards = new ArrayList<>();
         ownedCards.add(new ArrayList<>());
         ownedCards.add(new ArrayList<>());
         ownedCards.add(new ArrayList<>());
         faithTrackPosition = 0;
         popeFavorCards = new HashMap<>();
-        leaderCards = new ArrayList<>();
+        leaderCards = new HashMap<>();
+        leaderPowerStates = new HashMap<>();
         events = new ArrayList<>();
         victoryPoints = 0;
         faithTrackPoints = 0;
@@ -59,5 +68,32 @@ public class PlayerState {
 
     public void setPopeFavorCards(HashMap<String, HashMap<Integer, PopeFavorCard>> popeFavorCards) {
         this.popeFavorCards = new HashMap<>(popeFavorCards);
+    }
+
+    public HashMap<Resource, Integer> getLeaderDepots() {
+        return leaderDepots;
+    }
+
+    public void updateLeaderCardDepositState(String leaderCardID, int leaderPowerIndex, HashMap<Resource, Integer> storedResources) {
+        leaderDepotsState.putIfAbsent(leaderCardID, new ArrayList<>());
+        ArrayList<HashMap<Resource, Integer>> leaderPowersDeposit = leaderDepotsState.get(leaderCardID);
+
+
+        if(leaderPowerIndex<leaderPowersDeposit.size()){
+            HashMap<Resource, Integer> oldStoredResources = leaderPowersDeposit.get(leaderPowerIndex);
+            for(Resource r: Resource.values()){
+                leaderDepots.put(r, leaderDepots.get(r) - oldStoredResources.getOrDefault(r, 0) + storedResources.getOrDefault(r, 0));
+            }
+            leaderPowersDeposit.set(leaderPowerIndex, storedResources);
+        }
+        else{
+            for(Resource r: storedResources.keySet()){
+                leaderDepots.put(r, leaderDepots.get(r) + storedResources.get(r));
+            }
+            for(int i = leaderPowersDeposit.size(); i<leaderPowerIndex; i++) {
+                leaderPowersDeposit.add(new HashMap<>());
+            }
+            leaderPowersDeposit.add(storedResources);
+        }
     }
 }
