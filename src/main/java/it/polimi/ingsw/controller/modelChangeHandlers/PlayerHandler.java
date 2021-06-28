33a39c818b6af16d8f560d351c8a95fee8dbfe79
LeonardoScapolanMI <leonardo.scapolan@mail.polimi.it>
2021-06-28 21:01:08 +0200

    made the clients receive only the active leader cards of other players

--- src/main/java/it/polimi/ingsw/controller/modelChangeHandlers/PlayerHandler.java
+++ src/main/java/it/polimi/ingsw/controller/modelChangeHandlers/PlayerHandler.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.events.ClientEvents.PlayerStateEvent;
 import it.polimi.ingsw.model.LeaderCards.LeaderCard;
 import it.polimi.ingsw.model.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -24,9 +25,20 @@ public class PlayerHandler extends MatchObserver{
         Player player = (Player) o;
 
         HashMap<String, Boolean> leaderCards = new HashMap<>();
-        for(LeaderCard lc: player.getLeaderCards())
-            leaderCards.put(lc.getCardID(), player.getActiveLeaderCards().contains(lc));
+        HashMap<String, Boolean> actLeaderCards = new HashMap<>();
+        ArrayList<LeaderCard> activeLeaderCards = player.getActiveLeaderCards();
+        for(LeaderCard lc: player.getLeaderCards()){
+            boolean active = activeLeaderCards.contains(lc);
+            leaderCards.put(lc.getCardID(), active);
+            if(active)
+                actLeaderCards.put(lc.getCardID(), true);
+        }
 
-        sendToAll(new PlayerStateEvent(player.getPlayerId(), leaderCards));
+        for(String playerID: networkData.keySet()){
+            if(player.getPlayerId().equals(playerID))
+                networkData.get(playerID).sendObject(new PlayerStateEvent(player.getPlayerId(), leaderCards));
+            else
+                networkData.get(playerID).sendObject(new PlayerStateEvent(player.getPlayerId(), actLeaderCards));
+        }
     }
 }
