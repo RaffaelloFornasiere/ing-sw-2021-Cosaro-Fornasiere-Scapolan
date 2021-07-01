bd4908020549c8bd36b02993b88499e55d62963a
raffaellofornasiere <fornasiere.raffaello@mail.polimi.it>
2021-07-01 11:45:24 +0200

    Javadoc on Controller and warnings fix

--- src/main/java/it/polimi/ingsw/ui/gui/Controller.java
+++ src/main/java/it/polimi/ingsw/ui/gui/Controller.java
@@ -2,30 +2,36 @@ package it.polimi.ingsw.ui.gui;
 
 import javafx.scene.Scene;
 
+
+/**
+ * abstract class for all gui controllers
+ */
 public abstract class Controller {
     protected GUI gui;
     protected boolean confirmable;
     Scene previousScene;
 
+    /**
+     * sets the scene that will call the one of the current controller.
+     * @param previousScene caller scene
+     */
     public void setPreviousScene(Scene previousScene) {
         this.previousScene = previousScene;
     }
 
+    /**
+     * sets a reference to the gui object
+     * @param gui reference object
+     */
     public void setGUI(GUI gui) {
         this.gui = gui;
     }
 
-    Controller() {
-    }
-
+    /**
+     * constructor: oblige all subclasses to provide the gui reference object
+     * @param gui reference object
+     */
     Controller(GUI gui) {
         this.gui = gui;
     }
-
-    Controller(GUI gui, boolean confirmable) {
-        this.gui = gui;
-        this.confirmable = confirmable;
-    }
-
-
 }
