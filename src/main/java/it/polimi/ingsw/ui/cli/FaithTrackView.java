3e1e7503fb699d963a47811ade2c4e14b126da46
LisaCosaro <lisa.cosaro@mail.polimi.it>
2021-07-02 04:22:06 +0200

    bettered the FaithTrackView

--- src/main/java/it/polimi/ingsw/ui/cli/FaithTrackView.java
+++ src/main/java/it/polimi/ingsw/ui/cli/FaithTrackView.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 public class FaithTrackView {
     private final HashMap<String, Integer> playersPositions;
     private final HashMap<String, HashMap<Integer, PopeFavorCard>> popeFavorCards;
-    ArrayList<DrawableObject> objs2= new ArrayList<>();
+
 
     /**
      * constructor, put all initial position to 0
@@ -37,6 +37,25 @@ public class FaithTrackView {
         }
 
 
+    }
+
+    /**
+     * method which displays the faithTrack and favorPopeCards of all players
+     *
+     * @param message       optional message to display
+     * @param currentPlayer the player who is visualizing the faithTrack
+     */
+    public void display(String message, String currentPlayer) {
+
+        String legend = "\033[31;1;4mLEGEND\033[0m \nNormal cell\n" +
+                Color.CYAN.getAnsiCode() + "VaticanSectionCell\n" + Color.reset() +
+                Color.RED.getAnsiCode() + "PopeCell\n" + Color.reset();
+        ArrayList<DrawableObject> objs = new ArrayList<>();
+        objs.add(new DrawableObject(message, 0, 0));
+        objs.add(new DrawableObject(legend, 0, 0));
+        Panel panel0 = new Panel(objs, System.out, false);
+        panel0.show();
+        ArrayList<DrawableObject> objs2= new ArrayList<>();
         GsonBuilder builder = new GsonBuilder();
         builder.registerTypeAdapter(AbstractCell.class, new GsonInheritanceAdapter<AbstractCell>());
         builder.registerTypeAdapter(EffectOfCell.class, new GsonInheritanceAdapter<EffectOfCell>());
@@ -81,25 +100,6 @@ public class FaithTrackView {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    /**
-     * method which displays the faithTrack and favorPopeCards of all players
-     *
-     * @param message       optional message to display
-     * @param currentPlayer the player who is visualizing the faithTrack
-     */
-    public void display(String message, String currentPlayer) {
-
-        String legend = "\033[31;1;4mLEGEND\033[0m \nNormal cell\n" +
-                Color.CYAN.getAnsiCode() + "VaticanSectionCell\n" + Color.reset() +
-                Color.RED.getAnsiCode() + "PopeCell\n" + Color.reset();
-        ArrayList<DrawableObject> objs = new ArrayList<>();
-        objs.add(new DrawableObject(message, 0, 0));
-        objs.add(new DrawableObject(legend, 0, 0));
-        Panel panel0 = new Panel(objs, System.out, false);
-        panel0.show();
-
 
             Panel panel = new Panel(objs2, System.out, true);
             panel.show();
